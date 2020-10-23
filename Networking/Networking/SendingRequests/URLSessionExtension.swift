88319d9fd4500e1eac22b2ab1344fb4e3d3f6567
Goran Pavlovic <pavlovicgoran94@gmail.com>
2020-10-23 07:57:18 +0200

    Add HTTPLoading Protocol Conform URLSession to HTTPLoading Protocol
    HTTPLoading protocol defines a spec for loading HTTP request with simple
    interface

---
+++ Networking/Networking/SendingRequests/URLSessionExtension.swift
@@ -0,0 +1,98 @@
+//
+//  URLSessionExtension.swift
+//  Networking
+//
+//  Created by Goran Pavlovic on 23/10/2020.
+//
+
+import Foundation
+
+extension URLSession: HTTPLoading {
+    #warning("Figure out a way to support canceling tasks")
+    public func load(request: HTTPRequest, completion: @escaping (HTTPResult) -> Void) {
+        // Try to construct the url
+        guard let url = request.url else {
+            let error = HTTPError(
+                code: .couldntBuildUrl,
+                request: request,
+                response: nil,
+                underlyingError: nil
+            )
+            completion(.failure(error))
+            return
+        }
+        
+        // construct url request
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = request.method.rawValue
+        
+        // copy over custom headers
+        for (header, value) in request.headers {
+            urlRequest.addValue(value, forHTTPHeaderField: header)
+        }
+        
+        
+        if request.body.isEmpty == false {
+            // copy over any additional data
+            for (header, value) in request.body.additionalHeaders {
+                urlRequest.addValue(header, forHTTPHeaderField: value)
+            }
+            
+            // try serializing body data
+            do {
+                urlRequest.httpBody = try request.body.encode()
+            } catch {
+                let httpError = HTTPError(
+                    code: .invalidBodySerialization,
+                    request: request,
+                    response: nil,
+                    underlyingError: nil
+                )
+                completion(.failure(httpError))
+                return
+            }
+        }
+        
+        let dataTask = self.dataTask(with: urlRequest) { (data, response, error) in
+            // parse response into the HTTPResult
+            let result = HTTPResult(request: request, responseData: data, response: response, error: error)
+            completion(result)
+        }
+        
+        dataTask.resume()
+    }
+}
+
+extension HTTPResult {
+    init(request: HTTPRequest, responseData: Data?, response: URLResponse?, error: Error?) {
+        var httpResponse: HTTPResponse?
+        if let r = response as? HTTPURLResponse {
+            httpResponse = HTTPResponse(request: request, response: r, body: responseData ?? Data())
+        }
+
+        if let e = error as? URLError {
+            #warning("Parse all HTTP Codes")
+            let code: HTTPError.Code
+            switch e.code {
+                case .badURL: code = .invalidRequest
+                case .unsupportedURL: code = .unknown
+                case .cannotFindHost: code = .unknown
+                default: code = .unknown
+            }
+            self = .failure(HTTPError(code: code, request: request, response: httpResponse, underlyingError: e))
+
+        } else if let someError = error {
+            // an error, but not a URL error
+            self = .failure(HTTPError(code: .unknown, request: request, response: httpResponse, underlyingError: someError))
+
+        } else if let response = httpResponse {
+            // not an error, and an HTTPURLResponse
+            #warning("Allow custom specific logic, since now http codes 505, 404 are resulting in successful case")
+            self = .success(response)
+
+        } else {
+            // not an error, but also not an HTTPURLResponse
+            self = .failure(HTTPError(code: .invalidResponse, request: request, response: nil, underlyingError: error))
+        }
+    }
+}
